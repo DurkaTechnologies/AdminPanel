b21ab09edda30d4380c36fa7a1c6e64621f8c697
Oleksiy Maksumchyk <lykabraine27@gmail.com>
2021-08-02 16:55:46 +0300

    IdentityContext and Seeds was added

---
+++ Infrastructure/Identity/Seeds/DefaultAdmin.cs
@@ -0,0 +1,41 @@
+﻿
+using Application.Enums;
+using Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity.Seeds
+{
+    public static class DefaultSuperAdminUser
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Default User
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "superadmin",
+                Email = "superadmin@gmail.com",
+                FirstName = "Mukesh",
+                LastName = "Murugan",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                IsActive = true
+            };
+
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "123Pa$$word!");
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Admin.ToString());
+                }
+            }
+        }
+    }
+}
