48233e605924e7b7bb1a5dc9ef70c32a89cc6be5
Oleksiy Maksumchyk <lykabraine27@gmail.com>
2021-08-15 21:14:48 +0300

    Admin Controllers were added

---
+++ WebUI/Areas/Admin/Models/RoleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Areas.Admin.Models
+{
+    public class RoleViewModel
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+
+    public class RoleClaimsViewModel
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public bool Selected { get; set; }
+    }
+}
---
+++ WebUI/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using AdminPanel.WebUI.Areas.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+        public int СommunityId { get; set; }
+
+        public string Email { get; set; }
+        
+        public bool IsActive { get; set; } = true;
+        
+        public string Password { get; set; }
+        
+        public string ConfirmPassword { get; set; }
+        
+        public bool EmailConfirmed { get; set; }
+        
+        public int CommunityId { get; set; }
+        
+        public CommunityViewModel Community { get; set; }
+    }
+}
